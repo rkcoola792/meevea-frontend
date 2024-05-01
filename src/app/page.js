7b0f0593efafcce5fa7159692d5dc38cdba9736a
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2024-05-01 13:59:13 +0530

    header made

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Header from "@/components/Header/Header";
+import Header2 from "@/components/Header/Header2";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1>Homepage</h1>
+    <h1>Home</h1>
   );
 }
